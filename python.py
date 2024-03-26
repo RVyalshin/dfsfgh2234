ed133fe1c29cd134473b9a5abf9f4305e5896ef4
PC <2006vrn@gmail.com>
2024-03-26 14:17:28 +0300

    First_Commit

--- python.py
+++ python.py
@@ -1,15 +1,1 @@
-def bubble_sort(array, len_array):
-    flag = True
-
-    while flag:
-        flag = False
-        for i in range(len_array - 1):
-            if array[i] > array[i + 1]:
-                array[i], array[i + 1] = array[i + 1], array[i]
-                flag = True
-
-    return array
-
-lis = [3, 1, 31425, 2, -1488, 1939, -float('inf')]
-
-print(bubble_sort(lis, len(lis)))
+print(123)
